d41ff6ae6af721bd376756de6365b6e5026193c1
Sandeep <chennasandeepreddy@gmail.com>
2025-11-06 00:07:44 +0530

    modified bed and chair objects

---
+++ src/src/Chair.java
@@ -0,0 +1,14 @@
+public class Chair {
+    private String color;
+    private String brand;
+    private int cods;
+    public Chair(){}
+    public Chair(String color,String brand, int cods){
+        this.color=color;
+        this.brand=brand;
+        this.cods=cods;
+    }
+    public Chair(String color){
+        this.color="red";
+    }
+}
