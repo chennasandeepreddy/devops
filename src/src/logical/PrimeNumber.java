9b4c95360d8c3b3dc7550c5774708c554a26dab0
Sandeep <chennasandeepreddy@gmail.com>
2025-11-06 18:24:37 +0530

    added logical code

---
+++ src/src/logical/PrimeNumber.java
@@ -0,0 +1,23 @@
+package logical;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public void primeOrNot(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to check Prime or Not");
+        int num=sc.nextInt();
+
+        for(int i=2;i<num;i++){
+            if(num%i==0){
+                System.out.println("The number "+num+" s not prime");
+                break;
+            }
+            else {
+                System.out.println("The number "+num+" is prime");
+                break;
+            }
+        }
+        sc.close();
+    }
+}
